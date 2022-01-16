d2bb268d47b50083b8bd5d69e39bbee0f6a140c1
Arnab Roy <royo.arnab@gmail.com>
2022-01-16 18:51:02 -0500

    Completed code to find distance between two coordinates

---
+++ calculate-distance.js
@@ -0,0 +1,28 @@
+/**
+ * This algorithm uses the haversine formula to calculate the distance between two GPS coordinates.
+ * It takes into consideration that the earth is spherical.
+ *
+ * @param {[lat1, lon1]} coordinate1 - Coordinates of the first gps location in degrees
+ * @param {[lat2, lon2]} coordinate2 - Coordinates of the second gps location in degrees
+ */
+const calculateDistance = (coordinate1, coordinate2) => {
+  let [lat1, lon1] = [...coordinate1];
+  let [lat2, lon2] = [...coordinate2];
+
+  // Convert latitudes and longitudes to radians
+  lat1 = (lat1 * Math.PI) / 180;
+  lat2 = (lat2 * Math.PI) / 180;
+
+  let longitudeDifference = ((lon2 - lon1) * Math.PI) / 180;
+  let latitudeDifference = ((lat2 - lat1) * Math.PI) / 180;
+
+  const R = 6371e3;
+
+  const a = Math.sin(latitudeDifference / 2) * Math.sin(latitudeDifference / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(longitudeDifference / 2) * Math.sin(longitudeDifference / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = Math.abs(R * c) / 1e3;
+
+  return distance;
+};
+
+
